9b10b6e71a1f6e1a3e1dc6d3eface99d9934c4f5
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-11-30 19:11:59 +0530

    Added Grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav""aside main"`,
+        }}
+        gridTemplateRows={"13vh 1fr 30px"}
+        gridTemplateColumns={[
+          "100vw 1fr",
+          "100vw 1fr",
+          "100vw 1fr",
+          "20vw 1fr",
+          "20vw 1fr",
+        ]}
+        h="100vh"
+        w="100vw"
+      >
+        <GridItem area="nav">Nav</GridItem>
+        <Show above="lg">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
+        <GridItem area="main">Main</GridItem>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
